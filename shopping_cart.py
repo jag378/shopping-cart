0ffa14d6610233632d5bdf2885a30ca5185aa75f
jag378 <46541182+jag378@users.noreply.github.com>
2019-02-04 22:14:46 -0500

    Add files via upload
    Updated to dollar format for individual products

--- shopping_cart.py
+++ shopping_cart.py
@@ -63,10 +63,13 @@ print(total_products)
 
 pre_tax = 0
 
+def dollar_format(prices):
+    return "${0:,.2f}".format(prices)
+
 for user_product in total_products:
     product_lookups = [p for p in products if str(p["id"]) == str(user_product)]
     product_lookup = product_lookups[0]
-    print(str(product_lookup["name"]) + ":  " + str(product_lookup["price"]))
+    print(str(product_lookup["name"]) + ":  " + str(dollar_format(product_lookup["price"])))
     pre_tax = pre_tax + product_lookup["price"]
 
 print("******************")
